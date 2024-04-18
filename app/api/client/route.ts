0715a8887f1ff761bb28af2e4c9e0ac67f707a34
Enzo Antunes <logonrmlocal@fiap.com.br>
2024-04-18 09:43:30 -0300

    Sei lá

--- app/api/client/route.ts
+++ app/api/client/route.ts
@@ -1,33 +1,33 @@
-import { Client } from "./interface"
 
-const clients: Client[] = [
+import { Client } from './interface';
+ 
+const clients: Client [] = [
     {
-        name: 'Tom Smith',
-        address: '123 Main St'
+        name: 'João Paulo',
+        address: 'Rua 1'
     },
     {
-        name: 'Jane Doe',
-        address: '456 Elm St'
+        name: 'Maria da Silva',
+        address: 'Rua 2'
     },
     {
-        name: 'John Smith',
-        address: '789 Oak St'
+        name: 'José Pereira',
+        address: 'Rua 3'
     }
-]
-
-export function GET() {
-    return Response.json({clients})
+];
+ 
+export function GET () {
+    return Response.json(clients);
 }
-
+ 
 export async function POST(request: Request) {
     const form = await request.formData();
     const name = form.get('name') as string;
     const address = form.get('address') as string;
-
-    clients.push({
-        name, 
-        address
+ 
+    clients.push({ name, address });
+    
+    return Response.json({
+        status: 'added',
     });
-
-   return Response.json({status: "Added"});
 }
